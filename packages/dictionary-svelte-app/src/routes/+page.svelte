7a5cac036202cb44407efb5e18c86221df8c8b69
Lucas Viana <contato@lucasviana.dev>
2023-02-19 14:50:27 -0300

    :construction: adds dictionary svelte project

---
+++ packages/dictionary-svelte-app/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
